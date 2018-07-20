a9b680e4b4665d589ad7debeec0a10b4e75f48e7
Martin Robertz <dream-master@gmx.net>
2018-07-20 22:42:26 +0200

    update version

--- src/main/java/bq_rf/core/BQRF.java
+++ src/main/java/bq_rf/core/BQRF.java
@@ -22,7 +22,7 @@ import cpw.mods.fml.common.registry.GameRegistry;
 public class BQRF
 {
     public static final String MODID = "bq_rf";
-    public static final String VERSION = "CI_MOD_VERSION";
+    public static final String VERSION = "1.0.13a";
     public static final String HASH = "CI_MOD_HASH";
     public static final String BRANCH = "CI_MOD_BRANCH";
     public static final String NAME = "RF Expansion";
